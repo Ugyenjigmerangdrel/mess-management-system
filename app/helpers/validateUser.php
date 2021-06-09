39bfc1c7fe7a688a9357dad093bdf63d512e9633
Ugyen Jigme Rangdrel <53612211+Ugyenjigmerangdrel@users.noreply.github.com>
2021-06-09 23:21:46 +0600

    Changes made to validate user

--- app/helpers/validateUser.php
+++ app/helpers/validateUser.php
@@ -4,11 +4,11 @@ function validateLogin($user){
     $errors = array();
 
     if (empty($user['username'])){
-        array_push($errors, true);
+        array_push($errors, "missing");
     }
 
     if (empty($user['password'])){
-        array_push($errors, true);
+        array_push($errors, "missing");
     }
 
     return $errors;
